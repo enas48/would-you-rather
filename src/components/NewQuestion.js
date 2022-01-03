911581b81f1987cd2e44d0e4c8fb1ddc992045b4
enas48 <enasm4829@gmail.com>
2022-01-03 14:19:47 +0200

    add JSON Server

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,5 +1,5 @@
 import React,{useState, useEffect} from 'react'
-import { Card, Form, Button,Divider} from 'semantic-ui-react'
+import { Card, Form, Button,Divider, Message } from 'semantic-ui-react'
 import  {handleAddQuestion} from '../actions/questions'
 import {Redirect } from 'react-router-dom'
 import {connect } from "react-redux";
@@ -7,8 +7,9 @@ import {activeNavItem} from '../actions/navItem'
 import PropTypes from 'prop-types';
 
 function NewQuestion(props) {
-  const [optionOne, setOptionOne]=useState("")
-  const [optionTwo, setOptionTwo]=useState("")
+  const [optionOne, setOptionOne]=useState('')
+  const [optionTwo, setOptionTwo]=useState('')
+  const [errors,setErrors]=useState({ optOne: [],optTwo: [],})
   const [toHome, setToHome]=useState(false)
   const {dispatch}=props
   useEffect(() => {
@@ -17,26 +18,87 @@ function NewQuestion(props) {
  
  const handleChange = (e) => {
    const {name, value} =e.target
+
+//    switch (name) {
+//     case 'optionOne': 
+//     errors.optOne = 
+//     value.length < 5 &&  value.length !==0
+//       ? 'option One must be 5 characters long!'
+//       : '';
+//   break;
+//   case 'optionTwo': 
+//   errors.optTwo = 
+//   value.length < 5 &&  value.length !==0
+//     ? 'option Two must be 5 characters long!'
+//     : '';
+// break;
+// default:
+//   break;
+//   }
+
    if(name==='optionOne'){
     setOptionOne(value );
+    if(value.length < 5 &&  value.length !==0){
+      errors.optOne=[]
+      errors.optOne.push('option One must be 5 characters long!') 
+    }
+    else{
+      errors.optOne=[]   
+    }
+    if(value === optionTwo  &&  value.length !==0){
+      errors.optOne.push('option mustn\'t be the same thing')  
+    } 
+   
+   
    }
    if(name==='optionTwo'){
     setOptionTwo(value );
+    if(value.length < 5 &&  value.length !==0){
+      errors.optTwo=[]
+      errors.optTwo.push('option Two must be 5 characters long!') 
+    }else{
+      errors.optTwo=[]
+    }
+    if(value === optionOne  &&  value.length !==0){
+      errors.optTwo.push('option mustn\'t be the same thing')  
+    } 
    }
+  //  if((value === optionOne || value === optionTwo) &&  (value.length !==0)){
+  //   errors.optOne=[]   
+  //   errors.optOne.push('option mustn\'t be the same thing') 
+  //   errors.optTwo.push('option mustn\'t be the same thing') 
+  // }
+
+   setErrors(errors)
   };
+
+  const validateForm = (errors) => {
+    let valid = true;
+    Object.values(errors).forEach(
+      // if we have an error string set valid to false
+      (val) => val.length > 0 && (valid = false)
+    );
+    return valid;
+  }
   const handleSubmit = (e) => {
     e.preventDefault();
     const{user, dispatch}=props
-    dispatch(handleAddQuestion(optionOne, optionTwo, user));
-    dispatch(activeNavItem("home"));
-    setOptionOne('')
-    setOptionTwo('')
-    setToHome(true)
+    if(validateForm(errors)) {
+      dispatch(handleAddQuestion(optionOne, optionTwo, user));
+      dispatch(activeNavItem("home"));
+      setOptionOne('')
+      setOptionTwo('')
+      setToHome(true)
+          }
+ 
+ 
   };
 
     if (toHome === true) {
       return <Redirect to="/" />;
     }
+     const optOneError=errors.optOne;
+     const optTwoError=errors.optTwo;
     return (
       <div className="col-lg-6 col-md-8 m-auto">
         <Card fluid>
@@ -61,6 +123,13 @@ function NewQuestion(props) {
                     onChange={handleChange}
                   />
                 </Form.Field>
+              {  optOneError.length!==0 &&<Message style={{display:  optOneError.length!==0 ? "block" : "none"}} size='mini'  negative>
+                  <Message.Header>
+                    <ul class="list">
+                      {optOneError.map((m,i)=><li key={i}>{m}</li>)}
+                    </ul>
+                  </Message.Header>
+                </Message>}
 
                 <Divider horizontal>Or</Divider>
                 <Form.Field>
@@ -70,6 +139,14 @@ function NewQuestion(props) {
                     value={optionTwo}
                     onChange={handleChange}
                   />
+            {  optTwoError.length!==0 &&<Message style={{display:  optTwoError.length!==0 ? "block" : "none"}} size='mini'  negative>
+                  <Message.Header>
+                    <ul class="list">
+                        {optTwoError.map((m,i)=><li key={i}>{m}</li>)}
+                    </ul>
+                    </Message.Header>
+           
+                </Message>}
                 </Form.Field>
                 <Button
                   basic
@@ -78,7 +155,7 @@ function NewQuestion(props) {
                   fluid
                   onClick={handleSubmit}
                   disabled={
-                    optionOne === "" || optionTwo === ""
+                    optionOne === '' || optionTwo === '' || optOneError.length!==0 || optTwoError.length!==0
                   }
                 >
                   Submit
